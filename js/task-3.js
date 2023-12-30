cf1d2529dc20690a2b342586b2f596b47de766d9
Ilya <kulak1224@gmail.com>
2023-12-30 20:40:50 +0200

    adding trim to input value

--- js/task-3.js
+++ js/task-3.js
@@ -2,8 +2,8 @@ const nameInput = document.querySelector('#name-input');
 const userName = document.querySelector('#name-output');
 
 nameInput.addEventListener('input', e => {
-  const inputVal = e.currentTarget.value;
-  if (!inputVal.trim()) {
+  const inputVal = e.currentTarget.value.trim();
+  if (!inputVal) {
     userName.textContent = 'Anonymous';
     return;
   }
