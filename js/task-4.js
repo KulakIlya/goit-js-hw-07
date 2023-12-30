d1243f9ffe0b3098cf37ca83b846ee0ed74c198a
Ilya <kulak1224@gmail.com>
2023-12-30 20:43:20 +0200

    adding trim to input value (4th task)

--- js/task-4.js
+++ js/task-4.js
@@ -3,10 +3,10 @@ const form = document.querySelector('.login-form');
 form.addEventListener('submit', e => {
   e.preventDefault();
 
-  const email = e.currentTarget.elements.email.value;
-  const password = e.currentTarget.elements.password.value;
+  const email = e.currentTarget.elements.email.value.trim();
+  const password = e.currentTarget.elements.password.value.trim();
 
-  if (!email.trim() || !password.trim()) return alert('All form fields must be filled in');
+  if (!email || !password) return alert('All form fields must be filled in');
 
   const formValues = {
     email,
